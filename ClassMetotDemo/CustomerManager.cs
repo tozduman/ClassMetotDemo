1b8ca134d0edafde916b69b96629b94b6bbb9bfc
Muzaffer Tozduman <tozduman@hotmail.com>
2021-02-12 18:14:21 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Eklendi");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi");
+        }
+
+        public void GetList(Customer customer)
+        {
+            Console.WriteLine("Müşteri " + customer.CustomerName);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+
+            customer.CustomerId = 1;
+            customer.CitizenId = "12345678901";
+            customer.CustomerName = "Engin";
+            customer.CustomerSurname = "Demiroğ";
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer);
+            customerManager.Delete(customer);
+            customerManager.GetList(customer);
+
+        }
+    }
+}
